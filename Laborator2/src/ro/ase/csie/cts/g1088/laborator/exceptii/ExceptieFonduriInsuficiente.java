01a2a5f9b08e83787839629b7a7afabc9f870c23
Irina Pro <Irina Pro@IRINA>
2021-03-04 12:31:36 +0200

    Lab 2

--- Laborator2/src/ro/ase/csie/cts/g1088/laborator/exceptii/ExceptieFonduriInsuficiente.java
+++ Laborator2/src/ro/ase/csie/cts/g1088/laborator/exceptii/ExceptieFonduriInsuficiente.java
@@ -1,7 +1,6 @@
 package ro.ase.csie.cts.g1088.laborator.exceptii;
 
 public class ExceptieFonduriInsuficiente extends Exception{
-	
 	public ExceptieFonduriInsuficiente(String mesaj) {
 		super(mesaj); //modul in care aperam const din clasa de baza 
 	}
--- Laborator2/src/ro/ase/csie/cts/g1088/laborator/exceptii/ExceptieTransferIlegal.java
+++ Laborator2/src/ro/ase/csie/cts/g1088/laborator/exceptii/ExceptieTransferIlegal.java
@@ -1,5 +1,5 @@
 package ro.ase.csie.cts.g1088.laborator.exceptii;
 
 public class ExceptieTransferIlegal extends Exception {
-
+// exceptie
 }

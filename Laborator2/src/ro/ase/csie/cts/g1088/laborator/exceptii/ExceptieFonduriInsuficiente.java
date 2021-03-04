6c17c29b0794eab0fee3b0be8f985175a5fe53c4
Irina Pro <Irina Pro@IRINA>
2021-03-04 12:24:25 +0200

    Laborator 2

---
+++ Laborator2/src/ro/ase/csie/cts/g1088/laborator/exceptii/ExceptieFonduriInsuficiente.java
@@ -0,0 +1,13 @@
+package ro.ase.csie.cts.g1088.laborator.exceptii;
+
+public class ExceptieFonduriInsuficiente extends Exception{
+	
+	public ExceptieFonduriInsuficiente(String mesaj) {
+		super(mesaj); //modul in care aperam const din clasa de baza 
+	}
+	
+	public ExceptieFonduriInsuficiente() {
+		
+	}
+
+}
